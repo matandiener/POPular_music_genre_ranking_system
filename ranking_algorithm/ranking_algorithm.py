f63c5b0b9a392fe58b4229287838f299d24c2720
matandiener <matandiener@gmail.com>
2017-09-28 01:05:15 +0300

    - small refactor to ranking_algorithm

--- ranking_algorithm/ranking_algorithm.py
+++ ranking_algorithm/ranking_algorithm.py
@@ -1,6 +1,9 @@
 from RankingData import YOUTUBE_KEY, BILLBOARD_KEY
 
 POPULAR_RANK_KEY = 'POPular_rank'
+VIEWS_COEFFICIENT = 0.6
+LIKES_AND_DISLIKES_COEFFICIENT = 0.3
+COMMENTS_COEFFICIENT = 0.1
 
 
 def rank_songs(songs):
@@ -8,18 +11,18 @@ def rank_songs(songs):
         songs[song][POPULAR_RANK_KEY] = rank_song(songs[song][YOUTUBE_KEY])
 
 
-def rank_song(song_data):
+def rank_song(song_youtube_data):
     total_views = 0
     total_like_and_dislikes = 0
     total_comments = 0
-    for youtube_vid in song_data:
+
+    for youtube_vid in song_youtube_data:
         total_views += youtube_vid.view_count if youtube_vid.view_count is not None else 0
         total_like_and_dislikes += youtube_vid.like_count if youtube_vid.like_count is not None else 0
         total_like_and_dislikes += youtube_vid.dislike_count if youtube_vid.dislike_count is not None else 0
         total_comments += youtube_vid.comment_count if youtube_vid.comment_count is not None else 0
 
-    alpha = 0.6
-    beta = 0.3
-    gama = 0.1
-
-    return alpha*total_views + beta*total_like_and_dislikes + gama*total_comments
+    return \
+        VIEWS_COEFFICIENT*total_views +\
+        LIKES_AND_DISLIKES_COEFFICIENT*total_like_and_dislikes +\
+        COMMENTS_COEFFICIENT*total_comments
